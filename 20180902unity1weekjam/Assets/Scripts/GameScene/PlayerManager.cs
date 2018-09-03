299d0790c930f4b2c5fbc3ca0abf4404ba8194bb
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-09-04 00:53:22 +0900

    【機能追加】マップ作成とプレイヤーの移動

---
+++ 20180902unity1weekjam/Assets/Scripts/GameScene/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.position += this.transform.forward * 0.1f;
+		}
+		else if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.position += this.transform.forward * -1 * 0.1f;
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.position += this.transform.right * 0.1f;
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.position += this.transform.right * -1 * 0.1f;
+		}
+	}
+}
